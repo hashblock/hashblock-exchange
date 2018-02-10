367cc5e62f20d91b88767417f0a322f24cb271c3
Arthur Greef <arthurgreef@live.com>
2018-02-10 11:41:27 -0800

    Fix namespace error.
    Align with namespace in units transaction processor.

--- cli/sawtooth_cli/unitsset.py
+++ cli/sawtooth_cli/unitsset.py
@@ -238,7 +238,7 @@ def _do_config_genesis(args):
 
 def _get_proposals(rest_client):
     state_leaf = rest_client.get_leaf(
-        _key_to_address('sawtooth.units.vote.proposals'))
+        _key_to_address('sawtooth.uom.vote.proposals'))
 
     config_candidates = UnitCandidates()
 
@@ -249,7 +249,7 @@ def _get_proposals(rest_client):
 
         candidates_bytes = None
         for entry in unit.entries:
-            if entry.key == 'sawtooth.units.vote.proposals':
+            if entry.key == 'sawtooth.uom.vote.proposals':
                 candidates_bytes = entry.value
 
         if candidates_bytes is not None:
@@ -376,9 +376,9 @@ def _config_inputs(key):
     given unit key.
     """
     return [
-        _key_to_address('sawtooth.units.vote.proposals'),
-        _key_to_address('sawtooth.units.vote.authorized_keys'),
-        _key_to_address('sawtooth.units.vote.approval_threshold'),
+        _key_to_address('sawtooth.uom.vote.proposals'),
+        _key_to_address('sawtooth.uom.vote.authorized_keys'),
+        _key_to_address('sawtooth.uom.vote.approval_threshold'),
         _key_to_address(key)
     ]
 
@@ -388,7 +388,7 @@ def _config_outputs(key):
     given unit key.
     """
     return [
-        _key_to_address('sawtooth.units.vote.proposals'),
+        _key_to_address('sawtooth.uom.vote.proposals'),
         _key_to_address(key)
     ]
 
