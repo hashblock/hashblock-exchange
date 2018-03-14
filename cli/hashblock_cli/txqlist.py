e3d8dbcc2ee4f17d0a1a8f13ef8961da0103898d
Frank V. Castellucci <frankiecast@gmail.com>
2018-03-14 12:06:57 -0400

    Added meta data inference from address to list result
    This is positional to get rid of the current assumptions when listing
    an off chain data query

--- cli/hashblock_cli/txqlist.py
+++ cli/hashblock_cli/txqlist.py
@@ -31,14 +31,58 @@ _RECIPROCATE_LIST_ADDRESS = _MATCH_NAMESPACE + \
     hashlib.sha512('mtxq'.encode("utf-8")).hexdigest()[0:6]
 
 _QUERY_KEY_MAP = {
-    'ask': _INITIATE_LIST_ADDRESS,
-    'tell': _RECIPROCATE_LIST_ADDRESS,
-    'offer': _INITIATE_LIST_ADDRESS,
-    'accept': _RECIPROCATE_LIST_ADDRESS,
-    'commitment': _RECIPROCATE_LIST_ADDRESS,
-    'obligation': _INITIATE_LIST_ADDRESS,
-    'give': _INITIATE_LIST_ADDRESS,
-    'take': _RECIPROCATE_LIST_ADDRESS}
+    'ask': _INITIATE_LIST_ADDRESS +
+    hashlib.sha512('ask'.encode("utf-8")).hexdigest()[0:6],
+    'tell': _RECIPROCATE_LIST_ADDRESS +
+    hashlib.sha512('tell'.encode("utf-8")).hexdigest()[0:6],
+    'offer': _INITIATE_LIST_ADDRESS +
+    hashlib.sha512('offer'.encode("utf-8")).hexdigest()[0:6],
+    'accept': _RECIPROCATE_LIST_ADDRESS +
+    hashlib.sha512('accept'.encode("utf-8")).hexdigest()[0:6],
+    'commitment': _RECIPROCATE_LIST_ADDRESS +
+    hashlib.sha512('commitment'.encode("utf-8")).hexdigest()[0:6],
+    'obligation': _INITIATE_LIST_ADDRESS +
+    hashlib.sha512('obligation'.encode("utf-8")).hexdigest()[0:6],
+    'give': _INITIATE_LIST_ADDRESS +
+    hashlib.sha512('give'.encode("utf-8")).hexdigest()[0:6],
+    'take': _RECIPROCATE_LIST_ADDRESS +
+    hashlib.sha512('take'.encode("utf-8")).hexdigest()[0:6]}
+
+
+# Addresses are string length 70 where:
+# Chars 0-5 are family (e.g. hashblock_match vs. hashblock_units, etc.)
+# Chars 6-11 are class (e.g. UTXQ vs. MTXQ)
+# Chars 12-17 are subtypes (e.g. ask vs. tell, etc.)
+
+_META_FAMILY = {
+    _MATCH_NAMESPACE: _ADDRESS_PREFIX
+}
+
+_META_CLASS = {
+    _INITIATE_LIST_ADDRESS: 'UTXQ',
+    _RECIPROCATE_LIST_ADDRESS: 'MTXQ'
+}
+
+_META_SUBTYPE = {
+    _QUERY_KEY_MAP['ask']: 'ask',
+    _QUERY_KEY_MAP['tell']: 'tell',
+    _QUERY_KEY_MAP['offer']: 'offer',
+    _QUERY_KEY_MAP['accept']: 'accept',
+    _QUERY_KEY_MAP['commitment']: 'commitment',
+    _QUERY_KEY_MAP['obligation']: 'obligation',
+    _QUERY_KEY_MAP['give']: 'give',
+    _QUERY_KEY_MAP['take']: 'take'
+}
+
+
+def __meta_dictionary(address):
+    meta_dict = {'family': None, 'class': 'all', 'subtype': 'all'}
+    meta_dict['family'] = _META_FAMILY[address[0:6]]
+    if len(address) >= 12:
+        meta_dict['class'] = _META_CLASS[address[0:12]]
+    if len(address) >= 18:
+        meta_dict['subtype'] = _META_SUBTYPE[address[0:18]]
+    return meta_dict
 
 
 def __get_query_address(listing_type):
@@ -54,17 +98,20 @@ def __get_query_address(listing_type):
     return qadd
 
 
-def __instance_for(signature, edata):
-    if signature[:12] == _INITIATE_LIST_ADDRESS:
+def __instance_for(address, edata):
+    if address[:12] == _INITIATE_LIST_ADDRESS:
         data = UTXQ()
         data.ParseFromString(b64decode(edata))
-    elif signature[:12] == _RECIPROCATE_LIST_ADDRESS:
+    elif address[:12] == _RECIPROCATE_LIST_ADDRESS:
         data = MTXQ()
         data.ParseFromString(b64decode(edata))
     else:
         print("Unknown data type in state")
         data = None
-    return dict([('instance', data), ('event_id', signature)])
+    return dict([
+        ('instance', data),
+        ('event_id', address),
+        ('meta_data', __meta_dictionary(address))])
 
 
 def exchange_list_for(listing_type, format_type, rest_client):
