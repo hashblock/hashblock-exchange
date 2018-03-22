deda7dd7ae8b7a7af649808e0a6cf8c9fdbf8e9b
Arthur Greef <arthurgreef@live.com>
2018-03-21 21:59:28 -0700

    Noninteractive
    Multi-line description of commit,
    feel free to be detailed.

--- network/azure/scripts/configure.0.sh
+++ network/azure/scripts/configure.0.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+USER="lambda";
+HOMEDIR="/home/$USER";
+CONFIG_LOG_FILE_PATH="$HOMEDIR/config.log";
+ARTIFACTS_URL_PREFIX="https://raw.githubusercontent.com/hashblock/hashblock-exchange/master/docker/compose";
+
 sudo apt-get -y update
 sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get -y update
@@ -7,17 +12,16 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get -y update
-sudo apt-get -y install docker-ce
+sudo "DEBIAN_FRONTEND=noninteractive apt-get -y install docker-ce"
+groupadd docker
+usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-USER="lambda";
-HOMEDIR="/home/$USER";
-CONFIG_LOG_FILE_PATH="$HOMEDIR/config.log";
-ARTIFACTS_URL_PREFIX="https://raw.githubusercontent.com/hashblock/hashblock-exchange/master/docker/compose";
+
 
 cd $HOMEDIR;
-wget -N ${ARTIFACTS_URL_PREFIX}/hashblock-node.0.yaml;
+sudo -u $USER /bin/bash -c "wget -N ${ARTIFACTS_URL_PREFIX}/hashblock-node.0.yaml";
 
 FAILED_EXITCODE=0;
 docker-compose -f hashblock-node.0.yaml up;
