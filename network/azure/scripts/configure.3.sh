35fde588178b90b3546c4ee5d9c44a4f8980639a
Arthur Greef <arthurgreef@live.com>
2018-03-22 12:27:08 -0700

    Fix docker-compose bug
    Move -d to the end of the statement.

--- network/azure/scripts/configure.3.sh
+++ network/azure/scripts/configure.3.sh
@@ -27,7 +27,7 @@ cd $HOMEDIR;
 sudo -u $USER /bin/bash -c "wget -N ${ARTIFACTS_URL_PREFIX}/hashblock-node.3.yaml";
 
 FAILED_EXITCODE=0;
-docker-compose -d -f hashblock-node.3.yaml up;
+docker-compose -f hashblock-node.3.yaml up -d;
 
 FAILED_EXITCODE=$?
 if [ $FAILED_EXITCODE -ne 0 ]; then
