2207c90b37b22ab25db0806c5d4e1b56b50b856c
Frank V. Castellucci <frankiecast@gmail.com>
2018-05-18 07:08:57 -0400

    start for packaging hashblock_zksnark

---
+++ zkSNARK/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name='hashblock_zksnark',
+    version='0.1.0',  # specified elsewhere
+    packages=[''],
+    package_dir={'': '.'},
+    package_data={'': ['hashblock_zksnark.so']},
+)
