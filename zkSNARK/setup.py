51ab4f1dfeab6aea5cb4e78837ef6d21c32cd855
Frank V. Castellucci <frankiecast@gmail.com>
2018-05-18 07:39:07 -0400

    Continued prep for zksnark extension installing

--- zkSNARK/setup.py
+++ zkSNARK/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='hashblock_zksnark',
     version='0.1.0',  # specified elsewhere
-    packages=[''],
-    package_dir={'': '.'},
-    package_data={'': ['hashblock_zksnark.so']},
+    packages=['hashblock_zksnark'],
+    package_dir={'hashblock_zksnark': '/hashblock-exchange/zkSNARK/hashblock_zksnark'},
+    package_data={'hashblock_zksnark': ['hashblock_zksnark.so']},
 )
