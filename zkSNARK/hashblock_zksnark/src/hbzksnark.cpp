8941269fd92bdb41861aa96c1dc18060565618a5
Frank V. Castellucci <frankiecast@gmail.com>
2018-05-26 12:14:08 -0400

    Remove profiling of generate

--- zkSNARK/hashblock_zksnark/src/hbzksnark.cpp
+++ zkSNARK/hashblock_zksnark/src/hbzksnark.cpp
@@ -19,24 +19,24 @@ using namespace libsnark;
 int main(int argc, const char * argv[])
 {
     default_r1cs_ppzkadsnark_pp::init_public_params();
-    libff::start_profiling();
+    //libff::start_profiling();
 
-    libff::enter_block("Generate Hashblock R1CS");
+    //libff::enter_block("Generate Hashblock R1CS");
     match_r1cs<libff::Fr<snark_pp<default_r1cs_ppzkadsnark_pp>>> r1cs = generate_match_r1cs<libff::Fr<snark_pp<default_r1cs_ppzkadsnark_pp>>>();
-    libff::leave_block("Generate Hashblock R1CS");
+    //libff::leave_block("Generate Hashblock R1CS");
 
-    libff::enter_block("Call to generate keys");
+    //libff::enter_block("Call to generate keys");
 
     r1cs_ppzkadsnark_auth_keys<default_r1cs_ppzkadsnark_pp> auth_keys = r1cs_ppzkadsnark_auth_generator<default_r1cs_ppzkadsnark_pp>();
 
-    libff::print_header("Hashblock R1CS ppzkADSNARK Generator");
+    //libff::print_header("Hashblock R1CS ppzkADSNARK Generator");
     r1cs_ppzkadsnark_keypair<default_r1cs_ppzkadsnark_pp> keypair = r1cs_ppzkadsnark_generator<default_r1cs_ppzkadsnark_pp>(r1cs.constraint_system,auth_keys.pap);
-    printf("\n"); libff::print_indent(); libff::print_mem("after generator");
+    //printf("\n"); libff::print_indent(); libff::print_mem("after generator");
 
-    libff::print_header("Preprocess verification key");
+    //libff::print_header("Preprocess verification key");
     r1cs_ppzkadsnark_processed_verification_key<default_r1cs_ppzkadsnark_pp> pvk = r1cs_ppzkadsnark_verifier_process_vk<default_r1cs_ppzkadsnark_pp>(keypair.vk);
 
-    libff::enter_block("Write encoded keys");
+    //libff::enter_block("Write encoded keys");
     std::stringstream pkss;
     std::stringstream vkss;
     pkss << keypair.pk ;
@@ -46,8 +46,8 @@ int main(int argc, const char * argv[])
     std::string svk = vkss.str();
     std::string encoded_svk = base64_encode(reinterpret_cast<const unsigned char*>(svk.c_str()), svk.length());
 
-    std::cout << "pk: " << encoded_spk << std::endl;
-    std::cout << "vk: " << encoded_svk << std::endl;
+    //std::cout << "pk: " << encoded_spk << std::endl;
+    //std::cout << "vk: " << encoded_svk << std::endl;
 
     std::ofstream file_pk("zkSNARK.pk");
     std::ofstream file_vk("zkSNARK.vk");
@@ -55,8 +55,8 @@ int main(int argc, const char * argv[])
     file_pk << encoded_spk;
     file_vk << encoded_svk;
 
-    libff::leave_block("Write encoded keys");
+    //libff::leave_block("Write encoded keys");
 
-    libff::leave_block("Call to generate keys");
+    //libff::leave_block("Call to generate keys");
 }
 
