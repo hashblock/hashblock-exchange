646fa3a7415688de9999e9bf702aae7ca6f8f658
Frank V. Castellucci <frankiecast@gmail.com>
2018-05-27 09:24:11 -0400

    Restructured and added pathing for generate in hbzksnark

--- zkSNARK/hashblock_zksnark/src/hbzksnark.cpp
+++ zkSNARK/hashblock_zksnark/src/hbzksnark.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <vector>
 #include <fstream>
+#include <iostream>
 
 #include <libff/common/profiling.hpp>
 
@@ -16,27 +17,12 @@
 
 using namespace libsnark;
 
-int main(int argc, const char * argv[])
-{
+void generate_keys(std::string file_path) {
     default_r1cs_ppzkadsnark_pp::init_public_params();
-    //libff::start_profiling();
-
-    //libff::enter_block("Generate Hashblock R1CS");
     match_r1cs<libff::Fr<snark_pp<default_r1cs_ppzkadsnark_pp>>> r1cs = generate_match_r1cs<libff::Fr<snark_pp<default_r1cs_ppzkadsnark_pp>>>();
-    //libff::leave_block("Generate Hashblock R1CS");
-
-    //libff::enter_block("Call to generate keys");
-
     r1cs_ppzkadsnark_auth_keys<default_r1cs_ppzkadsnark_pp> auth_keys = r1cs_ppzkadsnark_auth_generator<default_r1cs_ppzkadsnark_pp>();
-
-    //libff::print_header("Hashblock R1CS ppzkADSNARK Generator");
     r1cs_ppzkadsnark_keypair<default_r1cs_ppzkadsnark_pp> keypair = r1cs_ppzkadsnark_generator<default_r1cs_ppzkadsnark_pp>(r1cs.constraint_system,auth_keys.pap);
-    //printf("\n"); libff::print_indent(); libff::print_mem("after generator");
-
-    //libff::print_header("Preprocess verification key");
     r1cs_ppzkadsnark_processed_verification_key<default_r1cs_ppzkadsnark_pp> pvk = r1cs_ppzkadsnark_verifier_process_vk<default_r1cs_ppzkadsnark_pp>(keypair.vk);
-
-    //libff::enter_block("Write encoded keys");
     std::stringstream pkss;
     std::stringstream vkss;
     pkss << keypair.pk ;
@@ -46,17 +32,43 @@ int main(int argc, const char * argv[])
     std::string svk = vkss.str();
     std::string encoded_svk = base64_encode(reinterpret_cast<const unsigned char*>(svk.c_str()), svk.length());
 
-    //std::cout << "pk: " << encoded_spk << std::endl;
-    //std::cout << "vk: " << encoded_svk << std::endl;
-
-    std::ofstream file_pk("zkSNARK.pk");
-    std::ofstream file_vk("zkSNARK.vk");
+    std::string prvkey("hashblock_zkSNARK.pk");
+    std::string valkey("hashblock_zkSNARK.vk");
+    std::ofstream file_pk(file_path + prvkey);
+    std::ofstream file_vk(file_path + valkey);
 
     file_pk << encoded_spk;
     file_vk << encoded_svk;
 
-    //libff::leave_block("Write encoded keys");
+}
 
-    //libff::leave_block("Call to generate keys");
+int main(int argc, const char * argv[]) {
+
+    if (argc < 3) {
+        std::cerr <<  "Invalid catll. hbzksnark [-g, -p, -v] [options]" << std::endl;
+        return -1;
+    }
+    else if (strcmp(argv[1], "-g") == 0) {
+        if (argc > 3) {
+            std::cerr << "Invalid catll. hbzksnark -g file_path" << std::endl;
+            return -1;
+        }
+        else {
+            std::string file_path(argv[2]);
+            generate_keys(file_path);
+        }
+    }
+    else if (strcmp(argv[1], "-p") == 0) {
+
+    }
+    else if (strcmp(argv[1], "-v") == 0) {
+
+    }
+    else {
+        std::cerr <<  "No command match. Correct input and try again" << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
 
