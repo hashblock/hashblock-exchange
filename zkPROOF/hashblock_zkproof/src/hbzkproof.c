080f2402223fb3016a141cbc233388e323960440
Frank V. Castellucci <frankiecast@gmail.com>
2018-06-29 11:17:08 -0400

    Trouble with the verify

--- zkPROOF/hashblock_zkproof/src/hbzkproof.c
+++ zkPROOF/hashblock_zkproof/src/hbzkproof.c
@@ -49,6 +49,7 @@ static void counting_illegal_callback_fn(const char* str, void* data) {
 typedef struct {
 
 	//	Penderson commit vars and outcomes
+	secp256k1_context 				*none_ctx;
 	secp256k1_context 				*sign_ctx;
 	secp256k1_context 				*verify_ctx;
 	secp256k1_context 				*both_ctx;
@@ -67,17 +68,27 @@ typedef struct {
 	size_t 							extra_commit_len; // fixed 0 for no extra
 	int 							proof_len; // In buffer size, out length in buffer proof
 	unsigned char 					proof[5134+1];
+
+	//	Range proof verifies
+	int 							mantissa;
+	uint64_t 						verify_min;
+	uint64_t 						verify_max;
+
 } zkproof;
 
 int create_commitment(zkproof *zkp) {
-	int commit_res = secp256k1_pedersen_commit(zkp->sign_ctx, &zkp->commitment,
-		zkp->blind_ptr, zkp->value, secp256k1_generator_h);
+	int commit_res = secp256k1_pedersen_commit(
+		zkp->sign_ctx,
+		&zkp->commitment,
+		zkp->blind_ptr,
+		zkp->value,
+		secp256k1_generator_h);
 	return commit_res;
 }
 
 int create_proof(zkproof *zkp) {
 	int proof_res = secp256k1_rangeproof_sign(
-		zkp->both_ctx,
+		zkp->both_ctx, 		//	Needs to be context that has both signing and verifying
 		zkp->proof,
 		&zkp->proof_len,
 		zkp->min_value,
@@ -96,42 +107,71 @@ int create_proof(zkproof *zkp) {
 }
 
 int verify_proof(zkproof *zkp) {
-	int verify_res = 0;
+	int verify_res = secp256k1_rangeproof_verify(
+		zkp->both_ctx,
+		&zkp->verify_min,
+		&zkp->verify_max,
+		&zkp->commitment,
+		zkp->proof,
+		zkp->proof_len,
+		zkp->extra_commit,
+		zkp->extra_commit_len,
+		secp256k1_generator_h);
 	return verify_res;
+}
+
+int range_info(zkproof *zkp) {
+	int info_res = secp256k1_rangeproof_info(
+		zkp->none_ctx,
+		//		zkp->both_ctx,
+		&zkp->exponent,
+		&zkp->mantissa,
+		&zkp->verify_min,
+		&zkp->verify_max,
+		zkp->proof,
+		zkp->proof_len);
+	return info_res;
 }
 
 int main( int c , char *argv[]) {
 	(void)c;
 	(void)argv;
 	zkproof 	test;
+	test.none_ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
 	test.sign_ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
 	test.verify_ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
     test.both_ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
 	int32_t ecount=0;
 
+	secp256k1_context_set_error_callback(test.none_ctx, counting_illegal_callback_fn, &ecount);
 	secp256k1_context_set_error_callback(test.sign_ctx, counting_illegal_callback_fn, &ecount);
 	secp256k1_context_set_error_callback(test.verify_ctx, counting_illegal_callback_fn, &ecount);
 	secp256k1_context_set_error_callback(test.both_ctx, counting_illegal_callback_fn, &ecount);
+	secp256k1_context_set_illegal_callback(test.none_ctx, counting_illegal_callback_fn, &ecount);
     secp256k1_context_set_illegal_callback(test.sign_ctx, counting_illegal_callback_fn, &ecount);
     secp256k1_context_set_illegal_callback(test.verify_ctx, counting_illegal_callback_fn, &ecount);
     secp256k1_context_set_illegal_callback(test.both_ctx, counting_illegal_callback_fn, &ecount);
 
-    const unsigned char blind[32] = "   i am not a blinding factor   ";
+    //const unsigned char blind[32] = "   i am not a blinding factor   ";
+    const unsigned char blind[32] = "1c9f957deb830979bc414864bbf20c5e";
     test.blind_ptr = blind;
     test.value = 80;
 
     // Create a signed commitment for value N
     int cresult = create_commitment(&test);
-    printf("Commitment result = %i\n", cresult);
-    printf("Commitment buffer = %s\n", test.commitment.data);
-    printf("Commitment ecount = %i\n", ecount);
+    printf("Commitment result = %i\nCommitment = ", cresult);
+    for(int i=0; i < sizeof(test.commitment.data); i++)
+    	printf("%x", test.commitment.data[i]);
+    //printf("Commitment buffer = %s\n", test.commitment.data);
+    printf("\nCommitment ecount = %i\n\n", ecount);
 
     unsigned char message[120] = "When I see my own likeness in the depths of someone else's consciousness,  I always experience a moment of panic.";
     test.message_ptr = message;
     test.message_len = sizeof(message);
     test.nonce = test.commitment.data;
     test.min_value = 40;
-    test.min_bits = test.exponent = 0;
+    test.min_bits = 0;
+    test.exponent = 1;
     test.extra_commit = NULL;
     test.extra_commit_len = 0;
     test.proof_len = sizeof(test.proof);
@@ -139,8 +179,34 @@ int main( int c , char *argv[]) {
     // Create a signed proof
     int presult = create_proof(&test);
     printf("Proof result = %i\n", presult);
-    printf("Proof buffer size = %i\n", test.proof_len);
-    printf("Proof ecount = %i\n", ecount);
+    printf("Proof buffer size = %i\nProof = ", test.proof_len);
+    for(int i=0; i < test.proof_len; i++)
+    	printf("%x", test.proof[i]);
+    printf("\nProof ecount = %i\n\n", ecount);
+
+    //	Verify signed proof
+    //test.value = 50;
+    test.verify_min = 60;
+    test.verify_max = 100;
+
+    int vresult = verify_proof(&test);
+    printf("Verify result = %i\n", vresult);
+    printf("Verify value = %i\n", test.value);
+    printf("Verify min value = %i\n", test.verify_min);
+    printf("Verify max value = %i\n", test.verify_max);
+    printf("Verify ecount = %i\n\n", ecount);
+
+
+    //	Some information
+    test.verify_min = 60;
+    test.verify_max = 100;
+    int iresult = range_info(&test);
+    printf("Info result = %i\n", iresult);
+    printf("Info value = %i\n", test.value);
+    printf("Info min value = %i\n", test.verify_min);
+    printf("Info max value = %i\n", test.verify_max);
+    printf("Info ecount = %i\n\n", ecount);
+
 
 
     secp256k1_context_destroy(test.sign_ctx);
