8c5a9fab23c45c9b4673d2fc30be88de433ab691
Arthur Greef <arthurgreef@live.com>
2018-08-04 15:01:00 -0700

    Handing over the baton

--- zkPROC/hashblock_zkproc/src/hbzkproc.c
+++ zkPROC/hashblock_zkproc/src/hbzkproc.c
@@ -2,8 +2,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <secp256k1.h>
 
 int main( int c , char *argv[]) {
+
+	unsigned int flags =  SECP256K1_CONTEXT_SIGN;
+	secp256k1_context* ctx = secp256k1_context_create(flags);
+
+	char* turingsk = "f5109ec8e2b1bec395ca7e24e27aaf22bc0afaeef3e39314591fd488fa6805cd";
+
+	
+
 	printf("hello zk-protocol");
 	return 0;
 }
